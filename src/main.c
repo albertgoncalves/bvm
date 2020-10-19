ef5aea1e32ffee15cfe3c6b9975363d1b86bd4eb
albertgoncalves <goncalves.albert@gmail.com>
2020-10-19 19:47:58 -0400

    no unused functions -> big re-factor

--- src/main.c
+++ src/main.c
@@ -5,14 +5,308 @@
 
 typedef FILE           File;
 typedef struct termios TermIos;
+typedef tcflag_t       TcFlag;
 
 static TermIos TERMINAL;
+
+static void set_flags(Register r) {
+    if (REG[r] == 0) {
+        REG[R_COND] = FL_ZERO;
+    } else if (REG[r] >> 15) {
+        /* NOTE: A `1` in the left-most bit indicates negative. */
+        REG[R_COND] = FL_NEG;
+    } else {
+        REG[R_COND] = FL_POS;
+    }
+}
+
+static Bool poll_keyboard(void) {
+    FdSet file_descriptors;
+    FD_ZERO(&file_descriptors);
+    FD_SET(STDIN_FILENO, &file_descriptors);
+    TimeVal timeout;
+    timeout.tv_sec = 0;
+    timeout.tv_usec = 0;
+    return select(1, &file_descriptors, NULL, NULL, &timeout) != 0;
+}
+
+static u16 get_mem_at(u16 address) {
+    if (address == KEYBOARD_STATUS) {
+        if (poll_keyboard()) {
+            MEM[KEYBOARD_STATUS] = (1 << 15);
+            MEM[KEYBOARD_DATA] = (u16)getchar();
+        } else {
+            MEM[KEYBOARD_STATUS] = 0;
+        }
+    }
+    return MEM[address];
+}
+
+static void do_op_branch(u16 instr) {
+    // | 15| 14| 13| 12| 11| 10| 9 | 8 | 7 | 6 | 5 | 4 | 3 | 2 | 1 | 0 |
+    // +---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+
+    // | 0   0   0   0 | N | Z | P |             PC_OFFSET             |
+    // +---------------+---+---+---+-----------------------------------+
+    if (REG[R_COND] & get_r0_or_nzp(instr)) {
+        REG[R_PC] = (u16)(REG[R_PC] + get_pc_offset_9(instr));
+    }
+}
+
+static void do_op_add(u16 instr) {
+    u8 r0 = get_r0_or_nzp(instr);
+    u8 r1 = get_r1(instr);
+    if (get_immediate_mode(instr)) {
+        // | 15| 14| 13| 12| 11| 10| 9 | 8 | 7 | 6 | 5 | 4 | 3 | 2 | 1 | 0 |
+        // +---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+
+        // | 0   0   0   1 |     R0    |     R1    | 1 |     IMMEDIATE     |
+        // +---------------+-----------+-----------+---+-------------------+
+        REG[r0] = (u16)(REG[r1] + get_immediate(instr));
+    } else {
+        // | 15| 14| 13| 12| 11| 10| 9 | 8 | 7 | 6 | 5 | 4 | 3 | 2 | 1 | 0 |
+        // +---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+
+        // | 0   0   0   1 |     R0    |     R1    | 0 |  NULL |     R2    |
+        // +---------------+-----------+-----------+---+-------+-----------+
+        REG[r0] = (u16)(REG[r1] + REG[get_r2(instr)]);
+    }
+    set_flags(r0);
+}
+
+static void do_op_load(u16 instr) {
+    // | 15| 14| 13| 12| 11| 10| 9 | 8 | 7 | 6 | 5 | 4 | 3 | 2 | 1 | 0 |
+    // +---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+
+    // | 0   0   1   0 |     R0    |             PC_OFFSET             |
+    // +---------------+-----------+-----------------------------------+
+    u8 r0 = get_r0_or_nzp(instr);
+    REG[r0] = get_mem_at((u16)(REG[R_PC] + get_pc_offset_9(instr)));
+    set_flags(r0);
+}
+
+static void do_op_store(u16 instr) {
+    // | 15| 14| 13| 12| 11| 10| 9 | 8 | 7 | 6 | 5 | 4 | 3 | 2 | 1 | 0 |
+    // +---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+
+    // | 0   0   1   1 |     R0    |             PC_OFFSET             |
+    // +---------------+-----------+-----------------------------------+
+    MEM[REG[R_PC] + get_pc_offset_9(instr)] = REG[get_r0_or_nzp(instr)];
+}
+
+static void do_op_jump_subroutine(u16 instr) {
+    REG[R_7] = REG[R_PC];
+    if (get_relative_mode(instr)) {
+        // | 15| 14| 13| 12| 11| 10| 9 | 8 | 7 | 6 | 5 | 4 | 3 | 2 | 1 | 0 |
+        // +---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+
+        // | 0   1   0   0 | 1 |                 PC_OFFSET                 |
+        // +---------------+---+-------------------------------------------+
+        REG[R_PC] = (u16)(REG[R_PC] + get_pc_offset_11(instr));
+    } else {
+        // | 15| 14| 13| 12| 11| 10| 9 | 8 | 7 | 6 | 5 | 4 | 3 | 2 | 1 | 0 |
+        // +---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+
+        // | 0   1   0   0 | 0 |  NULL |     R1    |          NULL         |
+        // +---------------+---+-------+-----------+-----------------------+
+        REG[R_PC] = REG[get_r1(instr)];
+    }
+}
+
+static void do_op_and(u16 instr) {
+    u8 r0 = get_r0_or_nzp(instr);
+    u8 r1 = get_r1(instr);
+    if (get_immediate_mode(instr)) {
+        // | 15| 14| 13| 12| 11| 10| 9 | 8 | 7 | 6 | 5 | 4 | 3 | 2 | 1 | 0 |
+        // +---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+
+        // | 0   1   0   1 |     R0    |     R1    | 1 |     IMMEDIATE     |
+        // +---------------+-----------+-----------+---+-------------------+
+        REG[r0] = (u16)(REG[r1] & get_immediate(instr));
+    } else {
+        // | 15| 14| 13| 12| 11| 10| 9 | 8 | 7 | 6 | 5 | 4 | 3 | 2 | 1 | 0 |
+        // +---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+
+        // | 0   1   0   1 |     R0    |     R1    | 0 |  NULL |     R2    |
+        // +---------------+-----------+-----------+---+-------+-----------+
+        REG[r0] = (u16)(REG[r1] & REG[get_r2(instr)]);
+    }
+    set_flags(r0);
+}
+
+static void do_op_load_register(u16 instr) {
+    // | 15| 14| 13| 12| 11| 10| 9 | 8 | 7 | 6 | 5 | 4 | 3 | 2 | 1 | 0 |
+    // +---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+
+    // | 0   1   1   0 |     R0    |     R1    |         OFFSET        |
+    // +---------------+-----------+-----------+-----------------------+
+    u8 r0 = get_r0_or_nzp(instr);
+    REG[r0] = get_mem_at((u16)(REG[get_r1(instr)] + get_reg_offset_6(instr)));
+    set_flags(r0);
+}
+
+static void do_op_store_register(u16 instr) {
+    // | 15| 14| 13| 12| 11| 10| 9 | 8 | 7 | 6 | 5 | 4 | 3 | 2 | 1 | 0 |
+    // +---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+
+    // | 0   1   1   1 |     R0    |     R1    |         OFFSET        |
+    // +---------------+-----------+-----------+-----------------------+
+    MEM[REG[get_r1(instr)] + get_reg_offset_6(instr)] =
+        REG[get_r0_or_nzp(instr)];
+}
+
+static void do_op_not(u16 instr) {
+    // | 15| 14| 13| 12| 11| 10| 9 | 8 | 7 | 6 | 5 | 4 | 3 | 2 | 1 | 0 |
+    // +---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+
+    // | 1   0   0   1 |     R0    |     R1    |          NULL         |
+    // +---------------+-----------+-----------+-----------------------+
+    u8 r0 = get_r0_or_nzp(instr);
+    REG[r0] = (u16)(~REG[get_r1(instr)]);
+    set_flags(r0);
+}
+
+static void do_op_load_indirect(u16 instr) {
+    // | 15| 14| 13| 12| 11| 10| 9 | 8 | 7 | 6 | 5 | 4 | 3 | 2 | 1 | 0 |
+    // +---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+
+    // | 1   0   1   0 |     R0    |             PC_OFFSET             |
+    // +---------------+-----------+-----------------------------------+
+    u8 r0 = get_r0_or_nzp(instr);
+    REG[r0] =
+        get_mem_at(get_mem_at((u16)(REG[R_PC] + get_pc_offset_9(instr))));
+    set_flags(r0);
+}
+
+static void do_op_store_indirect(u16 instr) {
+    // | 15| 14| 13| 12| 11| 10| 9 | 8 | 7 | 6 | 5 | 4 | 3 | 2 | 1 | 0 |
+    // +---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+
+    // | 1   0   1   1 |     R0    |             PC_OFFSET             |
+    // +---------------+-----------+-----------------------------------+
+    MEM[get_mem_at((u16)(REG[R_PC] + get_pc_offset_9(instr)))] =
+        REG[get_r0_or_nzp(instr)];
+}
+
+static void do_op_jump(u16 instr) {
+    // | 15| 14| 13| 12| 11| 10| 9 | 8 | 7 | 6 | 5 | 4 | 3 | 2 | 1 | 0 |
+    // +---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+
+    // | 1   1   0   0 |    NULL   |     R1    |          NULL         |
+    // +---------------+-----------+-----------------------------------+
+    REG[R_PC] = REG[get_r1(instr)];
+}
+
+static void do_op_load_effective_address(u16 instr) {
+    // | 15| 14| 13| 12| 11| 10| 9 | 8 | 7 | 6 | 5 | 4 | 3 | 2 | 1 | 0 |
+    // +---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+
+    // | 1   1   1   0 |     R0    |             PC_OFFSET             |
+    // +---------------+-----------+-----------------------------------+
+    u8 r0 = get_r0_or_nzp(instr);
+    REG[r0] = (u16)(REG[R_PC] + get_pc_offset_9(instr));
+    set_flags(r0);
+}
+
+static void do_op_trap(u16 instr) {
+    // | 15| 14| 13| 12| 11| 10| 9 | 8 | 7 | 6 | 5 | 4 | 3 | 2 | 1 | 0 |
+    // +---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+
+    // | 1   1   1   1 |      NULL     |              TRAP             |
+    // +---------------+---------------+-------------------------------+
+    switch (get_trap(instr)) {
+    case TRAP_GETC: {
+        REG[R_0] = (u16)getchar();
+        break;
+    }
+    case TRAP_OUT: {
+        putc((char)REG[R_0], stdout);
+        fflush(stdout);
+        break;
+    }
+    case TRAP_PUTS: {
+        for (u16* x = MEM + REG[R_0]; *x; ++x) {
+            putc((char)*x, stdout);
+        }
+        fflush(stdout);
+        break;
+    }
+    case TRAP_IN: {
+        printf("Enter a character: ");
+        char x = (char)getchar();
+        putc(x, stdout);
+        REG[R_0] = (u16)x;
+        break;
+    }
+    case TRAP_PUTSP: {
+        for (u16* x = MEM + REG[R_0]; *x; ++x) {
+            char a = (char)((*x) & 0xFF);
+            putc(a, stdout);
+            char b = (char)((*x) >> 8);
+            if (b) {
+                putc(b, stdout);
+            }
+        }
+        fflush(stdout);
+        break;
+    }
+    case TRAP_HALT: {
+        puts("HALT");
+        fflush(stdout);
+        STATUS = DEAD;
+        break;
+    }
+    }
+}
+
+static void do_bin_instr(u16 instr) {
+    switch (get_op(instr)) {
+    case OP_BR: {
+        do_op_branch(instr);
+        break;
+    }
+    case OP_ADD: {
+        do_op_add(instr);
+        break;
+    }
+    case OP_LD: {
+        do_op_load(instr);
+        break;
+    }
+    case OP_ST: {
+        do_op_store(instr);
+        break;
+    }
+    case OP_JSR: {
+        do_op_jump_subroutine(instr);
+        break;
+    }
+    case OP_AND: {
+        do_op_and(instr);
+        break;
+    }
+    case OP_LDR: {
+        do_op_load_register(instr);
+        break;
+    }
+    case OP_STR: {
+        do_op_store_register(instr);
+        break;
+    }
+    case OP_NOT: {
+        do_op_not(instr);
+        break;
+    }
+    case OP_LDI: {
+        do_op_load_indirect(instr);
+        break;
+    }
+    case OP_STI: {
+        do_op_store_indirect(instr);
+        break;
+    }
+    case OP_JMP: {
+        do_op_jump(instr);
+        break;
+    }
+    case OP_LEA: {
+        do_op_load_effective_address(instr);
+        break;
+    }
+    case OP_TRAP: {
+        do_op_trap(instr);
+        break;
+    }
+    }
+}
 
 static void disable_input_buffering(void) {
     tcgetattr(STDIN_FILENO, &TERMINAL);
     TermIos terminal = TERMINAL;
-    terminal.c_lflag =
-        (tcflag_t)(terminal.c_lflag & (tcflag_t)(~ICANON & ~ECHO));
+    terminal.c_lflag = (TcFlag)(terminal.c_lflag & (TcFlag)(~ICANON & ~ECHO));
     tcsetattr(STDIN_FILENO, TCSANOW, &terminal);
 }
 
--- src/test.c
+++ src/test.c
@@ -2,11 +2,294 @@
 
 #include "vm.h"
 
+typedef struct {
+    i16    immediate_or_offset;
+    u8     r0_or_nzp;
+    u8     r1;
+    u8     r2;
+    Trap   trap;
+    Bool   mode;
+    OpCode op;
+} Instr;
+
 #define FAIL(test)               \
     {                            \
         printf("!\n" test "\n"); \
         exit(EXIT_FAILURE);      \
     }
+
+static void set_op(u16* instr, OpCode op) {
+    *instr = (u16)(*instr | (op << 12));
+}
+
+static void set_r0_or_nzp(u16* instr, u8 r0_or_nzp) {
+    *instr = (u16)(*instr | ((r0_or_nzp & 0x7) << 9));
+}
+
+static void set_r1(u16* instr, u8 r1) {
+    *instr = (u16)(*instr | ((r1 & 0x7) << 6));
+}
+
+static void set_r2(u16* instr, u8 r2) {
+    *instr = (u16)(*instr | (r2 & 0x7));
+}
+
+static void set_immediate(u16* instr, i8 immediate) {
+    *instr = (u16)(*instr | (1 << 5) | (immediate & 0x1F));
+}
+
+static void set_pc_offset_9(u16* instr, i16 pc_offset) {
+    *instr = (u16)(*instr | (pc_offset & 0x1FF));
+}
+
+static void set_relative_mode_and_pc_offset_11(u16* instr, i16 pc_offset) {
+    *instr = (u16)(*instr | (1 << 11) | (pc_offset & 0x7FF));
+}
+
+static void set_reg_offset_6(u16* instr, i16 pc_offset) {
+    *instr = (u16)(*instr | (pc_offset & 0x3F));
+}
+
+static void set_trap(u16* instr, Trap trap) {
+    *instr = (u16)(*instr | (trap & 0xFF));
+}
+
+static u16 get_op_branch(Instr instr) {
+    // | 15| 14| 13| 12| 11| 10| 9 | 8 | 7 | 6 | 5 | 4 | 3 | 2 | 1 | 0 |
+    // +---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+
+    // | 0   0   0   0 |    NZP    |             PC_OFFSET             |
+    // +---------------+-----------+-----------------------------------+
+    u16 bin_instr = 0;
+    set_op(&bin_instr, OP_BR);
+    set_r0_or_nzp(&bin_instr, instr.r0_or_nzp);
+    set_pc_offset_9(&bin_instr, instr.immediate_or_offset);
+    return bin_instr;
+}
+
+static u16 get_op_add(Instr instr) {
+    // | 15| 14| 13| 12| 11| 10| 9 | 8 | 7 | 6 | 5 | 4 | 3 | 2 | 1 | 0 |
+    // +---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+
+    // |               |           |           | 1 |     IMMEDIATE     |
+    // | 0   0   0   1 |     R0    |     R1    +---+-------+-----------|
+    // |               |           |           | 0 |  NULL |     R2    |
+    // +---------------+-----------+-----------+---+-------+-----------+
+    u16 bin_instr = 0;
+    set_op(&bin_instr, OP_ADD);
+    set_r0_or_nzp(&bin_instr, instr.r0_or_nzp);
+    set_r1(&bin_instr, instr.r1);
+    if (instr.mode) {
+        set_immediate(&bin_instr, (i8)instr.immediate_or_offset);
+    } else {
+        set_r2(&bin_instr, instr.r2);
+    }
+    return bin_instr;
+}
+
+static u16 get_op_load(Instr instr) {
+    // | 15| 14| 13| 12| 11| 10| 9 | 8 | 7 | 6 | 5 | 4 | 3 | 2 | 1 | 0 |
+    // +---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+
+    // | 0   0   1   0 |     R0    |             PC_OFFSET             |
+    // +---------------+-----------+-----------------------------------+
+    u16 bin_instr = 0;
+    set_op(&bin_instr, OP_LD);
+    set_r0_or_nzp(&bin_instr, instr.r0_or_nzp);
+    set_pc_offset_9(&bin_instr, instr.immediate_or_offset);
+    return bin_instr;
+}
+
+static u16 get_op_store(Instr instr) {
+    // | 15| 14| 13| 12| 11| 10| 9 | 8 | 7 | 6 | 5 | 4 | 3 | 2 | 1 | 0 |
+    // +---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+
+    // | 0   0   1   1 |     R0    |             PC_OFFSET             |
+    // +---------------+-----------+-----------------------------------+
+    u16 bin_instr = 0;
+    set_op(&bin_instr, OP_ST);
+    set_r0_or_nzp(&bin_instr, instr.r0_or_nzp);
+    set_pc_offset_9(&bin_instr, instr.immediate_or_offset);
+    return bin_instr;
+}
+
+static u16 get_op_jump_subroutine(Instr instr) {
+    // | 15| 14| 13| 12| 11| 10| 9 | 8 | 7 | 6 | 5 | 4 | 3 | 2 | 1 | 0 |
+    // +---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+
+    // |               | 1 |                 PC_OFFSET                 |
+    // | 0   1   0   0 +---+-------+-----------+-----------------------+
+    // |               | 0 |  NULL |     R1    |          NULL         |
+    // +---------------+---+-------+-----------+-----------------------+
+    u16 bin_instr = 0;
+    set_op(&bin_instr, OP_JSR);
+    if (instr.mode) {
+        set_relative_mode_and_pc_offset_11(&bin_instr,
+                                           instr.immediate_or_offset);
+    } else {
+        set_r1(&bin_instr, instr.r1);
+    }
+    return bin_instr;
+}
+
+static u16 get_op_and(Instr instr) {
+    // | 15| 14| 13| 12| 11| 10| 9 | 8 | 7 | 6 | 5 | 4 | 3 | 2 | 1 | 0 |
+    // +---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+
+    // |               |           |           | 1 |     IMMEDIATE     |
+    // | 0   1   0   1 |     R0    |     R1    +---+-------+-----------|
+    // |               |           |           | 0 |  NULL |     R2    |
+    // +---------------+-----------+-----------+---+-------+-----------+
+    u16 bin_instr = 0;
+    set_op(&bin_instr, OP_AND);
+    set_r0_or_nzp(&bin_instr, instr.r0_or_nzp);
+    set_r1(&bin_instr, instr.r1);
+    if (instr.mode) {
+        set_immediate(&bin_instr, (i8)instr.immediate_or_offset);
+    } else {
+        set_r2(&bin_instr, instr.r2);
+    }
+    return bin_instr;
+}
+
+static u16 get_op_load_register(Instr instr) {
+    // | 15| 14| 13| 12| 11| 10| 9 | 8 | 7 | 6 | 5 | 4 | 3 | 2 | 1 | 0 |
+    // +---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+
+    // | 0   1   1   0 |     R0    |     R1    |         OFFSET        |
+    // +---------------+-----------+-----------+-----------------------+
+    u16 bin_instr = 0;
+    set_op(&bin_instr, OP_LDR);
+    set_r0_or_nzp(&bin_instr, instr.r0_or_nzp);
+    set_r1(&bin_instr, instr.r1);
+    set_reg_offset_6(&bin_instr, instr.immediate_or_offset);
+    return bin_instr;
+}
+
+static u16 get_op_store_register(Instr instr) {
+    // | 15| 14| 13| 12| 11| 10| 9 | 8 | 7 | 6 | 5 | 4 | 3 | 2 | 1 | 0 |
+    // +---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+
+    // | 0   1   1   1 |     R0    |     R1    |         OFFSET        |
+    // +---------------+-----------+-----------+-----------------------+
+    u16 bin_instr = 0;
+    set_op(&bin_instr, OP_STR);
+    set_r0_or_nzp(&bin_instr, instr.r0_or_nzp);
+    set_r1(&bin_instr, instr.r1);
+    set_reg_offset_6(&bin_instr, instr.immediate_or_offset);
+    return bin_instr;
+}
+
+static u16 get_op_not(Instr instr) {
+    // | 15| 14| 13| 12| 11| 10| 9 | 8 | 7 | 6 | 5 | 4 | 3 | 2 | 1 | 0 |
+    // +---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+
+    // | 1   0   0   1 |     R0    |     R1    | 1 | 1 | 1 | 1 | 1 | 1 |
+    // +---------------+-----------+-----------+---+---+---+---+---+---+
+    u16 bin_instr = 0;
+    set_op(&bin_instr, OP_NOT);
+    set_r0_or_nzp(&bin_instr, instr.r0_or_nzp);
+    set_r1(&bin_instr, instr.r1);
+    set_reg_offset_6(&bin_instr, -1);
+    return bin_instr;
+}
+
+static u16 get_op_load_indirect(Instr instr) {
+    // | 15| 14| 13| 12| 11| 10| 9 | 8 | 7 | 6 | 5 | 4 | 3 | 2 | 1 | 0 |
+    // +---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+
+    // | 1   0   1   0 |     R0    |             PC_OFFSET             |
+    // +---------------+-----------+-----------------------------------+
+    u16 bin_instr = 0;
+    set_op(&bin_instr, OP_LDI);
+    set_r0_or_nzp(&bin_instr, instr.r0_or_nzp);
+    set_pc_offset_9(&bin_instr, instr.immediate_or_offset);
+    return bin_instr;
+}
+
+static u16 get_op_store_indirect(Instr instr) {
+    // | 15| 14| 13| 12| 11| 10| 9 | 8 | 7 | 6 | 5 | 4 | 3 | 2 | 1 | 0 |
+    // +---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+
+    // | 1   0   1   1 |     R0    |             PC_OFFSET             |
+    // +---------------+-----------+-----------------------------------+
+    u16 bin_instr = 0;
+    set_op(&bin_instr, OP_STI);
+    set_r0_or_nzp(&bin_instr, instr.r0_or_nzp);
+    set_pc_offset_9(&bin_instr, instr.immediate_or_offset);
+    return bin_instr;
+}
+
+static u16 get_op_jump(Instr instr) {
+    // | 15| 14| 13| 12| 11| 10| 9 | 8 | 7 | 6 | 5 | 4 | 3 | 2 | 1 | 0 |
+    // +---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+
+    // | 1   1   0   0 |    NULL   |     R1    |          NULL         |
+    // +---------------+-----------+-----------------------------------+
+    u16 bin_instr = 0;
+    set_op(&bin_instr, OP_JMP);
+    set_r1(&bin_instr, instr.r1);
+    return bin_instr;
+}
+
+static u16 get_op_load_effective_address(Instr instr) {
+    // | 15| 14| 13| 12| 11| 10| 9 | 8 | 7 | 6 | 5 | 4 | 3 | 2 | 1 | 0 |
+    // +---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+
+    // | 1   1   1   0 |     R0    |             PC_OFFSET             |
+    // +---------------+-----------+-----------------------------------+
+    u16 bin_instr = 0;
+    set_op(&bin_instr, OP_LEA);
+    set_r0_or_nzp(&bin_instr, instr.r0_or_nzp);
+    set_pc_offset_9(&bin_instr, instr.immediate_or_offset);
+    return bin_instr;
+}
+
+static u16 get_op_trap(Instr instr) {
+    // | 15| 14| 13| 12| 11| 10| 9 | 8 | 7 | 6 | 5 | 4 | 3 | 2 | 1 | 0 |
+    // +---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+
+    // | 1   1   1   1 |      NULL     |              TRAP             |
+    // +---------------+---------------+-------------------------------+
+    u16 bin_instr = 0;
+    set_op(&bin_instr, OP_TRAP);
+    set_trap(&bin_instr, instr.trap);
+    return bin_instr;
+}
+
+static u16 get_bin_instr(Instr instr) {
+    switch (instr.op) {
+    case OP_BR: {
+        return get_op_branch(instr);
+    }
+    case OP_ADD: {
+        return get_op_add(instr);
+    }
+    case OP_LD: {
+        return get_op_load(instr);
+    }
+    case OP_ST: {
+        return get_op_store(instr);
+    }
+    case OP_JSR: {
+        return get_op_jump_subroutine(instr);
+    }
+    case OP_AND: {
+        return get_op_and(instr);
+    }
+    case OP_LDR: {
+        return get_op_load_register(instr);
+    }
+    case OP_STR: {
+        return get_op_store_register(instr);
+    }
+    case OP_NOT: {
+        return get_op_not(instr);
+    }
+    case OP_LDI: {
+        return get_op_load_indirect(instr);
+    }
+    case OP_STI: {
+        return get_op_store_indirect(instr);
+    }
+    case OP_JMP: {
+        return get_op_jump(instr);
+    }
+    case OP_LEA: {
+        return get_op_load_effective_address(instr);
+    }
+    case OP_TRAP: {
+        return get_op_trap(instr);
+    }
+    }
+    exit(EXIT_FAILURE);
+}
 
 static void set_u16_to_string(char* buffer, u16 x) {
     u8 i = 15;
@@ -22,10 +305,6 @@ static void set_u16_to_string(char* buffer, u16 x) {
     }
 }
 
-static u8 get_neg(u16 instr) {
-    return (instr >> 11) & 0x1;
-}
-
 static void test_op_branch_neg(char* buffer) {
     Instr instr = {0};
     instr.op = OP_BR;
@@ -36,16 +315,12 @@ static void test_op_branch_neg(char* buffer) {
     if (strcmp(buffer, "0000 1001 1111 0110")) {
         FAIL("test_op_branch_neg (strcmp)");
     }
-    if ((instr.op != get_op(bin_instr)) || (!get_neg(bin_instr)) ||
+    if ((instr.op != get_op(bin_instr)) || (!((bin_instr >> 11) & 0x1)) ||
         (instr.immediate_or_offset != get_pc_offset_9(bin_instr)))
     {
         FAIL("test_op_branch_neg");
     }
     printf(".");
-}
-
-static u8 get_zero(u16 instr) {
-    return (instr >> 10) & 0x1;
 }
 
 static void test_op_branch_zero(char* buffer) {
@@ -58,16 +333,12 @@ static void test_op_branch_zero(char* buffer) {
     if (strcmp(buffer, "0000 0101 1100 1101")) {
         FAIL("test_op_branch_zero (strcmp)");
     }
-    if ((instr.op != get_op(bin_instr)) || (!get_zero(bin_instr)) ||
+    if ((instr.op != get_op(bin_instr)) || (!((bin_instr >> 10) & 0x1)) ||
         (instr.immediate_or_offset != get_pc_offset_9(bin_instr)))
     {
         FAIL("test_op_branch_zero");
     }
     printf(".");
-}
-
-static u8 get_pos(u16 instr) {
-    return (instr >> 9) & 0x1;
 }
 
 static void test_op_branch_pos(char* buffer) {
@@ -80,7 +351,7 @@ static void test_op_branch_pos(char* buffer) {
     if (strcmp(buffer, "0000 0011 1110 0101")) {
         FAIL("test_op_branch_pos (strcmp)");
     }
-    if ((instr.op != get_op(bin_instr)) || (!get_pos(bin_instr)) ||
+    if ((instr.op != get_op(bin_instr)) || (!((bin_instr >> 9) & 0x1)) ||
         (instr.immediate_or_offset != get_pc_offset_9(bin_instr)))
     {
         FAIL("test_op_branch_pos");
@@ -389,6 +660,22 @@ static void test_op_load_effective_address(char* buffer) {
     printf(".");
 }
 
+static void test_op_trap_halt(char* buffer) {
+    Instr instr = {0};
+    instr.op = OP_TRAP;
+    instr.trap = TRAP_HALT;
+    u16 bin_instr = get_bin_instr(instr);
+    set_u16_to_string(buffer, bin_instr);
+    if (strcmp(buffer, "1111 0000 0010 0101")) {
+        FAIL("test_op_trap_halt (strcmp)");
+    }
+    if ((instr.op != get_op(bin_instr)) || (instr.trap != get_trap(bin_instr)))
+    {
+        FAIL("test_op_trap_halt");
+    }
+    printf(".");
+}
+
 int main(void) {
     char* buffer = calloc(20, sizeof(char));
     if (buffer == NULL) {
@@ -412,6 +699,7 @@ int main(void) {
     test_op_store_register(buffer);
     test_op_not(buffer);
     test_op_load_effective_address(buffer);
+    test_op_trap_halt(buffer);
     free(buffer);
     printf("\nDone!\n");
     return EXIT_SUCCESS;
